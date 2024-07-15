817e118e6aacc05ea595a35e3900453e86a52778
jo <josehehall98@gmail.com>
2024-07-15 13:44:26 -0600

    inti

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { AppState } from './AppState'
+
+
+
+</script>
+
+<template>
+  <header>
+    <!-- <HomePage/> -->
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer class="bg-dark text-light">
+  </footer>
+</template>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+}
+</style>
